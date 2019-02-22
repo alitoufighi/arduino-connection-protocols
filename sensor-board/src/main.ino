467f2890dfeed8df1ca7cf47051937bfc92b24ca
TuRaMbARr <zangeneh.saeed411@yahoo.com>
2019-02-22 07:41:48 +0330

    Temprature sensor fixed

--- sensor-board/src/main.ino
+++ sensor-board/src/main.ino
@@ -1,20 +1,28 @@
-#define TempPin A0
+#define TempPin 0
 
 #include <Arduino.h>
 #include <Ultrasonic.h>
+#include <LM35.h>
+#include <SoftwareSerial.h>
 
 Ultrasonic ultrasonic1(7, 6);	// An ultrasonic sensor HC-04
+SoftwareSerial vSerial(8, 9);
+float referenceVoltage;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(A0, INPUT);
+  analogReference(INTERNAL);
+  referenceVoltage = 1.1;
+  pinMode(0, INPUT);
+  vSerial.begin(9600);
 }
 
 void loop() {
     int distance = ultrasonic1.read(CM);
     int val = analogRead(TempPin);
-    float mv = (val/1024.0)*5000; 
-    float temperature = mv/10;
+    float temperature = (val * referenceVoltage * 100 / 1024.0); 
+    vSerial.println(temperature);
+
     byte buf[2];
     byte* floatBuf;
     buf[0] = distance & 255;
